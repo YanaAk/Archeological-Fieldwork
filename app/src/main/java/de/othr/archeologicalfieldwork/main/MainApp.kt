eb03e86ecc1ecadb672750968e083eeefa633b96
Yana Akhmetshina <yana.akhmetshina@st.oth-regensburg.de>
2021-01-17 20:35:09 +0100

    #12: Added user store to application

--- app/src/main/java/de/othr/archeologicalfieldwork/main/MainApp.kt
+++ app/src/main/java/de/othr/archeologicalfieldwork/main/MainApp.kt
@@ -2,17 +2,22 @@ package de.othr.archeologicalfieldwork.main
 
 import android.app.Application
 import de.othr.archeologicalfieldwork.model.Site
+import de.othr.archeologicalfieldwork.model.UserMemStore
+import de.othr.archeologicalfieldwork.model.UserStore
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import java.util.concurrent.atomic.AtomicLong
 
 class MainApp : Application(), AnkoLogger {
 
+    lateinit var userStore: UserStore
     var sites = ArrayList<Site>()
     var sitesCounter = AtomicLong()
 
     override fun onCreate() {
         super.onCreate()
+
+        this.userStore = UserMemStore()
 
         info("Application started")
     }
