1b4b5f5cccb1ee7fcb99874ac995d2948d187745
Yana Akhmetshina <yana.akhmetshina@st.oth-regensburg.de>
2021-01-23 17:26:53 +0100

    #13: Settings view and presenter

--- app/src/main/java/de/othr/archeologicalfieldwork/views/settings/SettingsPresenter.kt
+++ app/src/main/java/de/othr/archeologicalfieldwork/views/settings/SettingsPresenter.kt
@@ -1,9 +1,29 @@
 package de.othr.archeologicalfieldwork.views.settings
 
 import de.othr.archeologicalfieldwork.views.BasePresenter
-import de.othr.archeologicalfieldwork.views.BaseView
 import org.jetbrains.anko.AnkoLogger
 
-class SettingsPresenter(view: BaseView) : BasePresenter(view), AnkoLogger {
-    
+class SettingsPresenter(view: SettingsView) : BasePresenter(view), AnkoLogger {
+
+    private var settingsView: SettingsView = view
+
+    fun getUserEmail() {
+        settingsView?.setAccountEmailAddress(app.userStore.getCurrentUser()?.email)
+    }
+
+    fun getTotalNumOfSites() {
+        settingsView?.setTotalNumberOfSites(app.siteStore.findAll().size)
+    }
+
+    fun getTotalNumOfVisitedSites() {
+        settingsView?.setTotalNumberOfVisitedSites(0) //TODO
+    }
+
+    fun doApply() {
+        val currentUser = app.userStore.getCurrentUser()
+        val accountEmail = settingsView?.getAccountEmail()
+        val accountPassword = settingsView.getAccountPassword()
+
+        app.userStore.updateUser(currentUser?.id, accountEmail, accountPassword)
+    }
 }
--- app/src/main/java/de/othr/archeologicalfieldwork/views/settings/SettingsView.kt
+++ app/src/main/java/de/othr/archeologicalfieldwork/views/settings/SettingsView.kt
@@ -1,14 +1,55 @@
 package de.othr.archeologicalfieldwork.views.settings
 
 import android.os.Bundle
+import android.view.Menu
 import de.othr.archeologicalfieldwork.R
 import de.othr.archeologicalfieldwork.views.BaseView
+import kotlinx.android.synthetic.main.activity_settings.*
 import org.jetbrains.anko.AnkoLogger
 
 class SettingsView : BaseView(), AnkoLogger {
 
+    lateinit var presenter: SettingsPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        presenter = initPresenter (SettingsPresenter(this)) as SettingsPresenter
+
+        init(settingsToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        presenter.getUserEmail()
+        presenter.getTotalNumOfSites()
+        presenter.getTotalNumOfVisitedSites()
+
+        settingsApply.setOnClickListener { presenter.doApply() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_blank, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun setTotalNumberOfSites(size: Int) {
+        settingsStatsSitesNum.text = size.toString()
+    }
+
+    fun setTotalNumberOfVisitedSites(size: Int) {
+        settingsStatsVisitedNum.text = size.toString()
+    }
+
+    fun setAccountEmailAddress(email: String?) {
+        settingsUsername.setText(email?.orEmpty())
+    }
+
+    fun getAccountEmail(): String {
+        return settingsUsername.text.toString()
+    }
+
+    fun getAccountPassword(): String {
+        return settingsPassword.text.toString()
     }
 }
